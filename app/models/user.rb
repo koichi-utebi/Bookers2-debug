b32ef0dee1492ec5e6be90635419c3ddd00d610c
“koichi-utebi <k.utebi.1225@gmail.com>
2022-04-20 06:48:47 +0000

    検索機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,20 @@ class User < ApplicationRecord
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
@@ -27,7 +41,7 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
