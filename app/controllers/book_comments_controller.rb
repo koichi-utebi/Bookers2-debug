a0fc22c7212a3afbb807e51dca3ca3a352132974
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-02 04:35:18 +0000

    コメント機能の非同期通信化開始

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,14 +4,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.find_by(book_id: book.id)
     comment.destroy
-    redirect_to book_path(book)
   end
 
   private
