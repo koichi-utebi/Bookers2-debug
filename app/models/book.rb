af507dee77737997d7dde3cc9c2b2fe7e5bc244b
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-05 05:03:16 +0000

    6終了

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,11 +5,8 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-<<<<<<< HEAD
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
-=======
-  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorites'
->>>>>>> origin
+
 
   def self.looks(search, word)
     if search == "perfect_match"
