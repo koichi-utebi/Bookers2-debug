33e69a234ff3faff7cef226c71d840a4d29e738f
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-06 06:33:02 +0000

    7b fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,8 +10,8 @@ class Book < ApplicationRecord
 
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-  scope :created_this_week, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }
-  scope :created_last_week, -> { where(created_at: ((Time.current.at_end_of_day - 13.day).at_beginning_of_day)..((Time.current.at_end_of_day - 7.day).at_beginning_of_day)) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
 
   def self.looks(search, word)
     if search == "perfect_match"
