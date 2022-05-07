4932746ebd99649b2019b689f130daa586ea0a76
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-07 05:28:21 +0000

    8b finish

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "rails-ujs"
+import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery"
+import jQuery from "jquery"
 import "popper.js"
 import "bootstrap"
+import Chart from 'chart.js/auto';
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+global.$ = jQuery;
+window.$ = jQuery;
+global.Chart = Chart;
